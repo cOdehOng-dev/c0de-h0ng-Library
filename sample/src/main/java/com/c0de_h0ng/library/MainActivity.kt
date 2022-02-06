e0b24e146f705ef55039749556cc9f809165093c
cOdehOng-dev <hsw0323@naver.com>
2022-02-06 23:36:29 +0900

    [Fix] DataBinding 버그 수정

--- sample/src/main/java/com/c0de_h0ng/library/MainActivity.kt
+++ sample/src/main/java/com/c0de_h0ng/library/MainActivity.kt
@@ -1,27 +1,24 @@
-package com.c0de_h0ng.library.sample
+package com.c0de_h0ng.library
 
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
-import com.c0de_h0ng.library.R
-import com.c0de_h0ng.library.base.BaseActivity
+import com.c0de_h0ng.library.base.BaseBindingActivity
 import com.c0de_h0ng.library.databinding.ActivityMainBinding
-import com.c0de_h0ng.library.util.ChlibLog
 
-class MainActivity : BaseActivity() {
+class MainActivity : BaseBindingActivity<ActivityMainBinding>() {
 
-//    override val layoutRes: Int
-//        get() = R.layout.activity_main
-//
-//    override fun bindingProperty() {
-//
-//    }
+    override val layoutRes: Int
+        get() = R.layout.activity_main
 
-    private lateinit var binding: ActivityMainBinding
+    override fun bindingProperty() {
+
+    }
+
+    //private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        ChlibLog.debug("바인딩 " + DataBindingUtil.setContentView(this, R.layout.activity_main))
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        //binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.textString = "!@#1323231"
         //bindingView<ActivityMainBinding>(R.layout.activity_main)
 //        val  s = compareCurrentInputDate("yyyy-MM-dd HH:mm:ss", "2021.02.05 23:50:00")
 //        ChlibLog.debug("날짜 $s")
