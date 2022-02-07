744a7e51d6f3dcc9b4bd887bd78599eee24fd791
cOdehOng-dev <hsw0323@naver.com>
2022-02-07 21:03:30 +0900

    [Feature] Base Adapter 추가

--- library/src/main/java/com/c0de_h0ng/library/base/BaseListAdapter.kt
+++ library/src/main/java/com/c0de_h0ng/library/base/BaseListAdapter.kt
@@ -4,35 +4,23 @@ import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 
 /**
- * Created by c0de_h0ng on 2022/02/06.
+ * Created by c0de_h0ng on 2022/02/07.
  */
-abstract class BaseViewPagerAdapter<T> constructor(
+abstract class BaseListAdapter<T> constructor(
     diffCallback: DiffUtil.ItemCallback<T>
 ) : ListAdapter<T, BaseViewHolder<T, *>>(diffCallback) {
-
 
     override fun onFailedToRecycleView(holder: BaseViewHolder<T, *>): Boolean {
         holder.itemView.animation?.cancel()
         return true
     }
 
-
     override fun onViewDetachedFromWindow(holder: BaseViewHolder<T, *>) {
         holder.unbind()
     }
 
     override fun onBindViewHolder(holder: BaseViewHolder<T, *>, position: Int) {
-        val itemList: List<T> by lazy { listOf(currentList.last()) + currentList + listOf(currentList.first()) }
-        holder.bind(itemList[position])
-    }
-
-    override fun getItemCount(): Int {
-        return if (currentList.isNotEmpty()) {
-            val itemList: List<T> by lazy { listOf(currentList.last()) + currentList + listOf(currentList.first()) }
-            itemList.count()
-        } else {
-            0
-        }
+        holder.bind(getItem(position))
     }
 
 }
