d0ecc9f983afca377b8e463753c6936969aad4a3
cOdehOng-dev <hsw0323@naver.com>
2022-02-07 20:48:31 +0900

    [Feature] 샘플 코드 추가

---
+++ sample/src/main/java/com/c0de_h0ng/library/sample/SampleRecyclerActivity.kt
@@ -0,0 +1,17 @@
+package com.c0de_h0ng.library.sample
+
+import android.os.Bundle
+import com.c0de_h0ng.library.base.BaseActivity
+
+/**
+ * Created by c0de_h0ng on 2022/02/07.
+ */
+class SampleRecyclerActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_sample)
+    }
+
+}
